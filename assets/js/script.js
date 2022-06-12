82a8fa5023c9cdc90ed7de25dfedf45339b4cef2
Katie <katiebutler37@gmail.com>
2022-06-12 13:45:07 -0400

    get lon and lat data from function to local storage to next function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+var searchHistory = [];
+var geoapify = "https://api.geoapify.com";
+var geoapifyApiKey = "5f6f470ded3b4d53b71e4c9a2385246a";
+var onecall = "https://api.openweathermap.org";
+var onecallApiKey = "0b0775930b7defc71f282858d288d0c6";
+
 var getCityCoordinates = function(city) {
     // format the geoapify api url
-    var geoapifyUrl = "https://api.geoapify.com/v1/geocode/search?city=" + city + "&apiKey=5f6f470ded3b4d53b71e4c9a2385246a";
+    var geoapifyUrl =  geoapify + "/v1/geocode/search?city=" + city + "&apiKey=" + geoapifyApiKey;
   
     // make a get request to url
     fetch(geoapifyUrl)
@@ -8,10 +14,13 @@ var getCityCoordinates = function(city) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
+              //grab data from first item in features array because they are ordered from highest to lowest popularity ranking
             var latitude = data.features[0].properties.lat;
             var longitude = data.features[0].properties.lon;
             console.log(latitude);
             console.log(longitude);
+            localStorage.setItem("latitude", JSON.stringify(latitude));
+            localStorage.setItem("longitude", JSON.stringify(longitude));
           });
         } else {
           alert('Error: City Not Found');
@@ -23,6 +32,28 @@ var getCityCoordinates = function(city) {
   };
   getCityCoordinates("Toronto");
 
-  var getWeatherData = function(latitude, longitude) {
-    var onecallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid={API key}"
-  }
+var savedlLatitude = localStorage.getItem("latitude").trim();
+var savedLongitude = localStorage.getItem("longitude").trim();
+
+  var getWeatherData = function(savedLatitude, savedLongitude) {
+      // format the onecall api url
+    var onecallUrl =  onecall + "/data/2.5/onecall?lat=" + savedLatitude + "&lon=" + savedLongitude + "&appid=" + onecallApiKey;
+       // make a get request to url
+       fetch(onecallUrl)
+       .then(function(response) {
+         // request was successful
+         if (response.ok) {
+             console.log(response);
+           response.json().then(function(data) {
+             console.log(data);
+           });
+         } else {
+           alert('Error: City Not Found');
+         }
+       })
+       .catch(function(error) {
+         alert('Unable to connect to server');
+       });
+  };
+  getWeatherData(savedlLatitude, savedLongitude);
+
